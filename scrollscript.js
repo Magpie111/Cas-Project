1a90af27553024bf05f777979b51d2b7cf0415a6
Magpie111 <91876899+Magpie111@users.noreply.github.com>
2022-06-23 00:49:54 +0200

    update1

--- scrollscript.js
+++ scrollscript.js
@@ -4,17 +4,29 @@ let maxsp = 100;
 
 var r = document.querySelector(':root');
 
+var NL = document.querySelectorAll('.Hlinks');
+console.log(NL)
 
 function doSomething(scrollPos) {
+    var NL = document.querySelectorAll('.Hlinks');
     console.log(scrollPos)
     document.getElementById("1").style.bottom = (30-10*Math.log10(scrollPos)).toString()+"vh"
     
     if (scrollPos != 0){
+
+      document.getElementById("h21").style.fontSize  = "3vh"
+      document.getElementById("header").style.height = "8vh"
+      NL.forEach(element => {
+        element.style.fontSize = '2.5vh';
+      });
+
+    }else{
+
       document.getElementById("header").style.height = "10vh"
       document.getElementById("h21").style.fontSize  = "5vh"
-    }else{
-      document.getElementById("header").style.height = "15vh"
-      document.getElementById("h21").style.fontSize  = "10vh"
+      NL.forEach(element => {
+        element.style.fontSize = '3vh';
+      });
     }
 }
 
